5861ad839daeaece9f90298c30e0c0b35cc4da07
Suraj jain <suraj.2125cs1087@kiet.edu>
2023-10-22 17:27:19 +0530

    first

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,36 @@
 import Features from "./components/Features/features"
+import { Route, Routes } from "react-router-dom"
 import Hero from "./components/Hero_Home/Hero"
 import Events from "./components/Events_display/Events_display"
+import Event_page from "./components/Event_page/event_page"
 
+// import {React} from 'react';
+// import {  Route, Switch } from 'react-router-dom';
+
+function Home(){
+
+  return(
+    <>
+    <Hero/>
+     <Features/>
+     <Events/>
+    </>
+  )
+}
 
 function App() {
   return (
     <>
-      <Hero/>
-      <Features/>
-      <Events/>
+    
+    <Routes>
+         <Route exact path='/' element={<Home/>}></Route>
+         <Route exact path= '/events_page' element = {<Event_page/>}></Route>
+    </Routes>
+      
+      {/* <Event_page/> */}
+
+      {/* <Event_page/> */}
+
     </>
   )
 }
--- my-react-app/src/components/Events_display/Event_details.jsx
+++ my-react-app/src/components/Events_display/Event_details.jsx
@@ -1,9 +1,19 @@
+import Event_page from "../Event_page/event_page"
+
 export default function Event_details(props){
+
     return(
         <>
-       <img src={image1} alt="" />
-        <div className="event_title">props.title</div>
-        <p className="event des">props.des</p>
+        <div className="event_details">
+       
+        <img src={props.icon} alt="" width="200px" height="270px" />
+      
+      <div className="img_down">
+        <div className="event_title">{props.title}</div>
+    <p className="event_des">{props.des}</p> 
+    </div>
+
+        </div> 
         </>
     )
 }
