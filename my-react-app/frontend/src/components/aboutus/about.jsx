d8de4e6af4b3712b1d76c9d96959c78207c4dad0
Suraj jain <suraj.2125cs1087@kiet.edu>
2024-05-18 03:16:39 +0530

    About us

--- my-react-app/frontend/src/components/aboutus/about.jsx
+++ my-react-app/frontend/src/components/aboutus/about.jsx
@@ -1,6 +1,7 @@
 import react from 'react'
 import './about.css'
-import suraj_img from '/suraj_img.png'
+// import suraj_img from '/suraj_img.png'
+import suraj_img from '/PROFESSIONAL PIC.jpg'
 
 export default function Aboutus(){
     return(
@@ -13,25 +14,31 @@ export default function Aboutus(){
             <div className="members_section">
                   <div className="member">
                      <div className="image">
-                         <img src={suraj_img} alt=''/>
+                         <img src={suraj_img} alt='' width="400px"/>
                      </div>
 
                      <div className="details">
                         <div className="name">KARAN VERMA</div>
-                        <div className="role">Frontend and Backend developer</div>
-                        <div className="more">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non voluptatum mollitia delectus cum officia reiciendis? Ipsam minus similique blanditiis enim sapiente necessitatibus in laboriosam harum aspernatur ipsum quam, est ducimus ea nostrum nihil voluptates?</div>
-                     </div>
+                        <div className="break"></div>
+                        <div className="role">UI UX DESIGNER | BACKEND DEVELOPER
+</div>
+                        <div className="more1">Lorem ipsum dolor sit amet.</div>
+                        <div className="more2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, deleniti?</div>                     </div>
                   </div>
+
+                  <div className="middle_line"></div>
 
                   <div className="member second">
                      <div className="image">
-                         <img src={suraj_img} alt=''/>
+                         <img src={suraj_img} alt='' width="400px" height="48px"/>
                      </div>
 
                      <div className="details">
                         <div className="name">SURAJ JAIN</div>
+                        <div className="break"></div>
                         <div className="role">Frontend and Backend developer</div>
-                        <div className="more">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non voluptatum mollitia delectus cum officia reiciendis? Ipsam minus similique blanditiis enim sapiente necessitatibus in laboriosam harum aspernatur ipsum quam, est ducimus ea nostrum nihil voluptates?</div>
+                        <div className="more1">Lorem ipsum dolor sit amet.</div>
+                        <div className="more2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, deleniti?</div>
                      </div>
                   </div>
             </div>
